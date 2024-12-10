085415c8bc307aa8bde1ee5e8e6c81cc5fae23cf
Saleh Zaidan <zaidan.saleh@outlook.com>
2024-12-10 21:17:13 +0700

    Implement Expected Sarsa on CliffWalking-v0

--- rl/td.py
+++ rl/td.py
@@ -64,3 +64,36 @@ def q_learning(
             done = terminated or truncated
 
     return np.argmax(Q, axis=1)
+
+
+def expected_sarsa(
+    env: gym.Env,
+    num_states: int,
+    num_actions: int,
+    discount_factor: float,
+    epsilon: float = 0.01,
+    alpha: float = 0.1,
+    max_episodes: int = 1_000,
+):
+    pi = np.ones((num_states, num_actions)) / num_actions
+    Q = np.zeros((num_states, num_actions))
+
+    for _ in range(max_episodes):
+        s, _ = env.reset()
+        done = False
+        while not done:
+            a = np.random.choice(range(num_actions), p=pi[s, :])
+            s_next, r, terminated, truncated, _ = env.step(a)
+            Q[s, a] += alpha * (
+                r + discount_factor * np.sum(pi[s_next, :] * Q[s_next, :]) - Q[s, a]
+            )
+            a_greedy = np.argmax(Q[s, :])
+            for a in range(num_actions):
+                if a == a_greedy:
+                    pi[s, a] = 1.0 - epsilon + epsilon / num_actions
+                else:
+                    pi[s, a] = epsilon / num_actions
+            s = s_next
+            done = terminated or truncated
+
+    return np.argmax(Q, axis=1)
